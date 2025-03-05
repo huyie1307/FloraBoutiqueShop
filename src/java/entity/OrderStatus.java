ca8a9aabccd27aa812b9574be5ac7210d312e993
Flamingowo <hming3010@gmail.com>
2025-03-05 08:18:54 +0700

    Orderstatus

---
+++ src/java/entity/OrderStatus.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+/**
+ *
+ * @author admin
+ */
+public class OrderStatus {
+    private int id;
+    private String statusName;
+    
+    public OrderStatus(){
+        
+    }
+    
+    public OrderStatus(int id){
+        this.id = id;
+    }
+
+    public OrderStatus(String statusName) {
+        this.statusName = statusName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStatusName() {
+        return statusName;
+    }
+
+    public void setStatusName(String statusName) {
+        this.statusName = statusName;
+    }
+    
+    
+
+}
