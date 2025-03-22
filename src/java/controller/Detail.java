5e2c83a2ff160c6ec0a2958829fc4af6e04c56c2
Admin <Admin@10.33.57.246>
2025-03-22 14:48:53 +0700

    update detail

--- src/java/controller/Detail.java
+++ src/java/controller/Detail.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(name = "Detail", urlPatterns = {"/productDetail"})
 public class Detail extends HttpServlet {
 
     @Override
